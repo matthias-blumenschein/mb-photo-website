96d7882e552e88cbc6015959b546c275af209d8d
Matthias Blumenschein <mb@nea-x.net>
2023-05-18 23:01:24 +0200

    feat: add navbar dummy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useRef } from "react";
+import Navbar from "./components/navbar/Navbar";
 import Hero from "./sections/hero/Hero";
 import Contact from "./sections/contact/Contact";
 import GalleryExt1 from "./sections/galleries/exterior/book1/GalleryExt1";
 import GalleryExt2 from "./sections/galleries/exterior/book2/GalleryExt2";
-import "./App.css";
 import GalleryInt1 from "./sections/galleries/interior/book1/GalleryInt1";
+import "./App.css";
 
 const HorizontalScroll = () => {
   const containerRef = useRef(null);
@@ -21,6 +22,7 @@ const HorizontalScroll = () => {
   return (
     <div className="appContainer" ref={containerRef} onWheel={handleScroll}>
       <main>
+        <Navbar />
         <Hero />
         <GalleryExt1 />
         <GalleryExt2 />
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
