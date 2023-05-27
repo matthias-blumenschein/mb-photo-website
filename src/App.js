2b9da93cce921fd30a3ddd46710b6b05113e0a52
Matthias Blumenschein <matthiasblumenschein@gmail.com>
2023-05-27 19:33:07 +0200

    feat: adjust horizontal scroll velocity depending on viewport width

--- src/App.js
+++ src/App.js
@@ -8,29 +8,32 @@ import GalleryInt1 from "./sections/galleries/interior/book1/GalleryInt1";
 import "./App.css";
 
 const HorizontalScroll = () => {
-  const containerRef = useRef(null);
+	const containerRef = useRef(null);
 
-  const handleScroll = (event) => {
-    const container = containerRef.current;
-    const scrollFactor = 4;
-    container.scrollTo({
-      left: container.scrollLeft + event.deltaY * scrollFactor,
-      behavior: "smooth",
-    });
-  };
+	const handleScroll = (event) => {
+		const container = containerRef.current;
+		const scrollFactor =
+			container.scrollLeftMax < 10000
+				? container.scrollLeftMax / 2500
+				: container.scrollLeftMax / 3500;
+		container.scrollTo({
+			left: container.scrollLeft + event.deltaY * scrollFactor,
+			behavior: "smooth",
+		});
+	};
 
-  return (
-    <div className="appContainer" ref={containerRef} onWheel={handleScroll}>
-      <main>
-        <Navbar />
-        <Hero />
-        <GalleryExt1 />
-        <GalleryExt2 />
-        <GalleryInt1 />
-        <Contact />
-      </main>
-    </div>
-  );
+	return (
+		<div className="appContainer" ref={containerRef} onWheel={handleScroll}>
+			<Navbar />
+			<main>
+				<Hero />
+				<GalleryExt1 />
+				<GalleryExt2 />
+				<GalleryInt1 />
+				<Contact />
+			</main>
+		</div>
+	);
 };
 
 export default HorizontalScroll;
